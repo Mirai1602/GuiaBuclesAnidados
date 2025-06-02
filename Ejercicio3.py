4214df0b03bc9765bacfaa740eac9d3fbfd75354
Jolespinoza-uam <melaespinoza@uamv.edu.ni>
2025-06-01 20:13:09 -0600

    Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -11,16 +11,16 @@ producto y muestra un resumen por stand, por día, y un total general de la feri
 def infoStands():
     stands = []
 
-    for i in range(2):
+    for i in range(4):
         stand = input(f"Ingrese el nombre del stand {i+1}: ")
         gananciasPorDia = []
         totalAcumulado = 0
 
-        for dia in range(2):
+        for dia in range(3):
             totalGananciaDia = 0
-            print(f"------Registro de ganancias del stand {stand}------")
+            print(f"───────Registro de ganancias del stand {stand}──────")
             print(f"                    DIA {dia+1}")
-            for j in range(2):
+            for j in range(3):
                 ganancia = int(input(f"Ingrese el monto de venta del producto {j+1}: "))
                 totalGananciaDia += ganancia
             gananciasPorDia.append(totalGananciaDia)
@@ -37,15 +37,24 @@ def infoStands():
     print("\n╔════════════════════════════════════════════════════╗")
     print("║            RESUMEN GANANCIAS - FERIA UAM           ║")
     print("╚════════════════════════════════════════════════════╝")
-    print("-----------------------------------------------------")
+    print("──────────────────────────────────────────────────────")
     print("Stand      | Día 1  | Día 2  | Día 3  | Total")
     for standInfo in stands:
         nombre = standInfo[0]
         ganancias = standInfo[1]
         total = standInfo[2]
-        print(f"Stand: {nombre}")
+        salida = f"{nombre}: "
         for i in range(len(ganancias)):
-             print({ganancias [i]})
-        print(f"    Total acumulado: {total}\n")
+             salida += f"| C${ganancias [i]}"
+        salida += f"| C${total}\n"
+        print(salida)
+    print("──────────────────────────────────────────────────────")
+
+    salidaTotal = f"{'TOTAL POR DÍA: '}"
+    for monto in totalPorDia:
+        salidaTotal += f"| C${monto}"
+    salidaTotal += f"| C$ {totalGeneral}"
+    print(salidaTotal)
+    print("──────────────────────────────────────────────────────")
 
 infoStands()
